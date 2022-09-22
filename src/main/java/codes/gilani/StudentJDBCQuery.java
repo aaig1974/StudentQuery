3723f77c0f7a389509775702ac30e9aa2c8ad5ec
aaig1974 <aaig74@gmail.com>
2022-09-22 23:49:01 +0200

    used @annotation, enhanced query

--- src/main/java/codes/gilani/StudentJDBCQuery.java
+++ src/main/java/codes/gilani/StudentJDBCQuery.java
@@ -8,6 +8,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class StudentJDBCQuery {
+    @SuppressWarnings("unused")
     Date dt = new Date();
     SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
     private static final String JDBC_DRIVER =  "com.mysql.cj.jdbc.Driver";
@@ -23,30 +24,31 @@ public class StudentJDBCQuery {
             Class.forName(JDBC_DRIVER);
             connection = DriverManager.getConnection(DATABASE_URL, USER_NAME, PASSWORD);
             statement = connection.createStatement();
-            String sqlQuery = "SELECT a.anrede,\n" +
-                    "       p.vorname,\n" +
-                    "       p.nachname,\n" +
-                    "       p.geb,\n" +
-                    "       adr.strasse,\n" +
-                    "       adr.haus_nr,\n" +
-                    "       o.plz,\n" +
-                    "       o.ort,\n" +
-                    "       bnd.bnd,\n" +
-                    "       f.stand,\n" +
-                    "       h.herst_name,\n" +
-                    "       m.modell_name,\n" +
-                    "       pr.preis FROM uebung_aufgabe.personen AS p\n" +
-                    "         INNER JOIN uebung_aufgabe.anrede AS a ON p.anrede_id = a.anrede_id\n" +
-                    "         INNER JOIN uebung_aufgabe.adresse AS adr ON p.adresse_id = adr.adresse_id\n" +
-                    "         INNER JOIN uebung_aufgabe.orte AS o ON adr.ort_id = o.ort_id\n" +
-                    "         INNER JOIN uebung_aufgabe.bundesland AS bnd ON adr.bnd_id = bnd.bnd_id\n" +
-                    "         INNER JOIN uebung_aufgabe.person_familenstand AS pf ON pf.perso_id = p.perso_id\n" +
-                    "         INNER JOIN uebung_aufgabe.familienstand AS f ON f.fam_id = pf.fam_id\n" +
-                    "         INNER JOIN uebung_aufgabe.besitzer AS b ON b.perso_id = p.perso_id\n" +
-                    "         INNER JOIN uebung_aufgabe.fahrzeuge AS kfz ON kfz.fahrzeug_id = b.fahrzeug_id\n" +
-                    "         INNER JOIN uebung_aufgabe.hersteller AS h ON kfz.hersteller_id = h.hersteller_id\n" +
-                    "         INNER JOIN uebung_aufgabe.modelle AS m ON kfz.modelle_id = m.modelle_id\n" +
-                    "         INNER JOIN uebung_aufgabe.preise AS pr ON kfz.preise_id = pr.preise_id;";
+            String sqlQuery = """
+                    SELECT a.anrede,
+                           p.vorname,
+                           p.nachname,
+                           p.geb,
+                           adr.strasse,
+                           adr.haus_nr,
+                           o.plz,
+                           o.ort,
+                           bnd.bnd,
+                           f.stand,
+                           h.herst_name,
+                           m.modell_name,
+                           pr.preis FROM uebung_aufgabe.personen AS p
+                             INNER JOIN uebung_aufgabe.anrede AS a ON p.anrede_id = a.anrede_id
+                             INNER JOIN uebung_aufgabe.adresse AS adr ON p.adresse_id = adr.adresse_id
+                             INNER JOIN uebung_aufgabe.orte AS o ON adr.ort_id = o.ort_id
+                             INNER JOIN uebung_aufgabe.bundesland AS bnd ON adr.bnd_id = bnd.bnd_id
+                             INNER JOIN uebung_aufgabe.person_familenstand AS pf ON pf.perso_id = p.perso_id
+                             INNER JOIN uebung_aufgabe.familienstand AS f ON f.fam_id = pf.fam_id
+                             INNER JOIN uebung_aufgabe.besitzer AS b ON b.perso_id = p.perso_id
+                             INNER JOIN uebung_aufgabe.fahrzeuge AS kfz ON kfz.fahrzeug_id = b.fahrzeug_id
+                             INNER JOIN uebung_aufgabe.hersteller AS h ON kfz.hersteller_id = h.hersteller_id
+                             INNER JOIN uebung_aufgabe.modelle AS m ON kfz.modelle_id = m.modelle_id
+                             INNER JOIN uebung_aufgabe.preise AS pr ON kfz.preise_id = pr.preise_id;""";
             resultSet = statement.executeQuery(sqlQuery);
             while (resultSet.next()) {
                 System.out.print(resultSet.getString("anrede") + ", ");
@@ -62,6 +64,7 @@ public class StudentJDBCQuery {
             e.printStackTrace();
         } finally {
             try {
+                assert resultSet != null;
                 resultSet.close();
                 statement.close();
                 connection.close();
